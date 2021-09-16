7eb22807d47e1e5463154ffcbc4be252dd209687
stefancosic93 <90192297+stefancosic93@users.noreply.github.com>
2021-09-16 14:57:50 +0200

    Create ScriptedStage.groovy

---
+++ src/org/demo/ScriptedStage.groovy
@@ -0,0 +1,19 @@
+class ScriptedStage {
+    private final Script script
+
+    ScriptedStage(Script script) {
+        this.script = script
+    }
+
+    // You can pass as many parameters as needed
+    void execute(String name, boolean param1) {
+        script.stage(name) {
+            script.echo "Triggering ${name} stage..."
+            script.sh "echo 'Execute your desired bash command here'"
+
+            if (param1) {
+                script.sh "echo 'Executing conditional command, because param1 == true'"
+            }
+        }
+    }
+}
