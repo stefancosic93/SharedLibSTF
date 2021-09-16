e2c8ec3e3aa523906e41dcf8e5235373e4489624
stefancosic93 <90192297+stefancosic93@users.noreply.github.com>
2021-09-16 15:30:40 +0200

    Update ScriptedStage.groovy

--- src/org/demo/ScriptedStage.groovy
+++ src/org/demo/ScriptedStage.groovy
@@ -6,21 +6,21 @@ class ScriptedStage {
     }
     
     void scmCheckout() {
-        script.stage("Checkout SCM") {
-            script.echo "Triggering Checkout SCM stage..."
-            script.bat "checkout scm"
-        }
+
     }
 
     // You can pass as many parameters as needed
-    void execute(String name, boolean param1) {
+    void execute(String name) {
+        if (name == "Checkout SCM") {
+                script.stage("Checkout SCM") {
+                    script.echo "Triggering Checkout SCM stage..."
+                    script.bat "checkout scm"
+            }
+        }
         script.stage(name) {
             script.echo "Triggering ${name} stage..."
            // script.sh "echo 'Execute your desired bash command here'"
             script.bat "mvn -v"
-            if (param1) {
-                script.bat "echo 'Executing conditional command, because param1 == true'"
-            }
         }
     }
     
