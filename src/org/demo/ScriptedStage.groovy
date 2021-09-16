b46cb03102d147b3541312735f5720b3556211c8
stefancosic93 <90192297+stefancosic93@users.noreply.github.com>
2021-09-16 15:46:33 +0200

    Update ScriptedStage.groovy

--- src/org/demo/ScriptedStage.groovy
+++ src/org/demo/ScriptedStage.groovy
@@ -9,26 +9,26 @@ class ScriptedStage {
     void execute(String name) {
         script.echo "Triggering ${name} stage..."
         if (name == "Checkout SCM") {
-                script.stage(name) {
-                    script.bat "checkout scm"
+            script.stage(name) {
+                script.bat "checkout scm"
             }
         }
         if (name == "Tests") {
-                script.stage(name) {
-                    script.bat "mvn test"
+            script.stage(name) {
+                script.bat "mvn test"
             }
         }
         if (name == "Sonar") {
-                script.stage(name) {
-                    script.bat "mvn clean verify sonar:sonar -Dsonar.login=f260730b8650aba93bb9cdad3310b95dbb1eec4e"
+            script.stage(name) {
+                script.bat "mvn clean verify sonar:sonar -Dsonar.login=f260730b8650aba93bb9cdad3310b95dbb1eec4e"
             }
         }
         if (name == "Artifactory") {
-                script.stage(name) {
-                    script.bat "echo implement artifactory"
+            script.stage(name) {
+                script.bat "echo implement artifactory"
             }
         }
-        script.stage(name) {
+        script.stage(name) { //Foo, delete later
             script.echo "Triggering ${name} stage..."
             script.bat "mvn -v"
         }
