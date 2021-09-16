1bc76e0d4eb5c7862fcc016304c37580af759fe8
stefancosic93 <90192297+stefancosic93@users.noreply.github.com>
2021-09-16 12:40:12 +0200

    Update buildUtils.groovy

--- src/org/demo/buildUtils.groovy
+++ src/org/demo/buildUtils.groovy
@@ -1,5 +1,13 @@
 package org.demo
+class Utilities implements Serializable {
+  def env
+  def steps
+  Utilities(env, steps) {
+    this.env = env
+    this.steps = steps
+  }
 
-def checkOutFrom(repo) {
-  git url: "git@github.com:jenkinsci/${repo}"
+  def mvn(args) {
+    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+  }
 }
