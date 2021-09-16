c2f199e9255fa36f0b19e1745c43c9996da8b322
stefancosic93 <90192297+stefancosic93@users.noreply.github.com>
2021-09-16 14:35:07 +0200

    Create UtilitiesSteps.groovy

---
+++ src/org/demo/UtilitiesSteps.groovy
@@ -0,0 +1,10 @@
+package org.demo
+class UtilitiesSteps implements Serializable {
+  def steps
+  Utilities(steps) {
+    this.steps = steps
+  }
+  def mvn(args) {
+    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+  }
+}
